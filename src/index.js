3b414b7cc09c7f5a49d64ab737ebb9b6f1213b3c
sanjay vasudeva <jaydevsocial@outlook.com>
2020-07-04 16:52:32 +0530

    Creating a new react component called hello

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <h1>Hello World</h1>
+  <Hello />
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
