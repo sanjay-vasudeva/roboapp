5fd821e02ea046bb94aed2bacd64f232deff435a
sanjay vasudeva <jaydevsocial@outlook.com>
2020-07-04 22:30:13 +0530

    robots

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Card = () => {
     return(
-    <div>
-        <img alt = "jane Doe" src = "https://robohash.org/asd"/>
+    <div className = "bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img alt = "jane Doe" 
+            src = "https://robohash.org/asd?size=200x200"/>
         <div>
             <h2>Jane Doe</h2>
             <p>janedoe@gmail.com</p>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import Card from './Card';
+import { Robots } from './Robots';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-  <Card />
-  </React.StrictMode>
-   ,
-  document.getElementById('root')
+  <div>
+    <Card id={Robots[0].id} name = {Robots[0].name} email = {Robots[0].email}/>
+    <Card id={Robots[1].id} name = {Robots[1].name} email = {Robots[1].email}/>
+    <Card id={Robots[2].id} name = {Robots[2].name} email = {Robots[2].email}/>
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
