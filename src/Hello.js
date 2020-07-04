8e6b36001d6b89a7a859a126794a379b41bebb8e
sanjay vasudeva <jaydevsocial@outlook.com>
2020-07-04 17:15:38 +0530

    added reference to hello.css

---
+++ src/Hello.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import '/.Hello.css';
+
+
+class Hello extends Component {
+    render() {
+      return (
+        <div class="Welcome">
+          <h1>Hello World</h1>
+          <p>Welcome to my first react app</p>
+        </div>
+      );
+    }
+  }
+  
+
+//   class Hello1 extends Component {
+//     render() {
+//       return(
+//       <h1>Hello World 1</h1>
+//     );
+//     }
+//   }
+  
+
+  export default Hello;
