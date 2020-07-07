3216f6b72b5392cb3a83b998c4d161aa1bb0f511
sanjay vasudeva <jaydevsocial@outlook.com>
2020-07-07 12:34:45 +0530

    Updates

---
+++ precache-manifest.1cc98596ea8600b17c1df3969e09fe17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac5dd865f45d2d64d9b73fee1693a3a",
+    "url": "/roboapp/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/roboapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cfa179b58d260912545f",
+    "url": "/roboapp/static/css/main.68a2b772.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/roboapp/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboapp/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa179b58d260912545f",
+    "url": "/roboapp/static/js/main.26853f53.chunk.js"
+  },
+  {
+    "revision": "d659b12d7e33e29f7db9",
+    "url": "/roboapp/static/js/runtime-main.ebd44ea6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
